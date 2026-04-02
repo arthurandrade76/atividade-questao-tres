b1e8346ae42a95059269ebe06d7df8b2c0180537
pizza <pizza@benaldo>
2026-04-02 19:02:37 -0300

    mid commit

---
+++ src/main/java/com/mycompany/inngressos/Normal.java
@@ -0,0 +1,10 @@
+package com.mycompany.inngressos;
+public class Normal extends Ingresso {
+public Normal(double valor) {
+super(valor);
+}
+
+public void imprimeTipo() {
+System.out.println("Ingresso Normal");
+}
+}
