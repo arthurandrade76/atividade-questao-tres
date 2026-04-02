5c4b4725e2960643da0aedebdbe3ee1eb641a27a
pizza <pizza@benaldo>
2026-04-02 19:07:01 -0300

    mid commit

---
+++ src/main/java/com/mycompany/inngressos/CamaroteInferior.java
@@ -0,0 +1,21 @@
+package com.mycompany.inngressos;
+public class CamaroteInferior extends VIP {
+private String localizacao;
+
+public CamaroteInferior(double valor, double valorAdicional, String localizacao) {
+super(valor, valorAdicional);
+this.localizacao = localizacao;
+}
+
+public String getLocalizacao() {
+return localizacao;
+}
+
+public void setLocalizacao(String localizacao) {
+this.localizacao = localizacao;
+}
+
+public void imprimeLocalizacao() {
+System.out.println("Localização: " + localizacao);
+}
+}
