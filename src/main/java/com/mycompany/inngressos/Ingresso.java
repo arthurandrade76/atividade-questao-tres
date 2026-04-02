aa5fbdfc52e20f11e9020a741e336fd4a192df15
pizza <pizza@benaldo>
2026-04-02 18:44:39 -0300

    initial commit

---
+++ src/main/java/com/mycompany/inngressos/Ingresso.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.inngressos;
+public class Ingresso {
+private double valor;
+
+public Ingresso(double valor) {
+this.valor = valor;
+}
+
+public void imprimeValor() {
+System.out.println("Valor do Ingresso: R$ " + valor);
+}
+
+public double getValor() {
+return valor;
+}
+
+public void setValor(double valor) {
+this.valor = valor;
+}
+}
+
